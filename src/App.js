bae5d88cea9fcb995be0ca1f38e7c4b3687d1aa0
LIMON029 <limon00029@gmail.com>
2022-07-21 19:12:10 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-
-  const onChangeToDo = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [...currentArray, toDo]);
-    setToDo("");
-  }
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [dollars, setDollars] = useState(0);
+  const [nowPrice, setNowPrice] = useState(1);
+  const [nowCoin, setNowCoin] = useState("");
 
   useEffect(() => {
-    if(toDos.length !== 0) {
-      console.log(toDos);
-    }
-  }, [toDos]);
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+        setNowPrice(json[0].quotes.USD.price);
+        setNowCoin(json[0].symbol);
+      });
+  }, []);
+
+  const onChangeDollar = (event) => {
+    setDollars(event.target.value);
+  };
+
+  const onSelection = (event) => {
+    setNowPrice(coins[event.target.value-1].quotes.USD.price);
+    setNowCoin(coins[event.target.value-1].symbol);
+  };
 
   return (
     <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input value={toDo} type="text" placeholder='Write your to do...' onChange={onChangeToDo}/>
-        <button>ADD</button>
-      </form>
-      <hr />
-      {toDos.map((item, index) =>
-        <li key={index}>{item}</li>
-      )}
+      <h1>The Coins{loading ? "" : `(${coins.length})`}</h1>
+      {loading
+        ? <strong>Loading...</strong>
+        : <div>
+            <input type="number" placeholder='Input the money to convert.' value={dollars} onChange={onChangeDollar}/>
+            <span> USD&nbsp;&nbsp;&nbsp;&nbsp;→&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <input type="number" placeholder='Result' value={dollars !== 0 ? dollars/nowPrice : 0} disabled/>
+            <span> {nowCoin}</span>
+            <select style={{"display": "block", marginTop: 10}} onChange={onSelection}>
+              {coins.map((coin) => <option value={coin.rank} key={coin.rank}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</option>)}
+            </select>
+          </div>}
     </div>
   );
 }
