f349b4147b23c09580e9b9e493094a9de0a345c3
LIMON029 <limon00029@gmail.com>
2022-07-27 23:47:50 +0900

    The end

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -24,7 +24,7 @@ function Navigation() {
     return (
         <div className="nav">
             <Link to="/" onClick={onClick}>Home</Link>
-            <Link to={url} onClick={onClickA}>About</Link>
+            <Link to={url} onClick={onClickA}>Last</Link>
         </div>
     );
 }
